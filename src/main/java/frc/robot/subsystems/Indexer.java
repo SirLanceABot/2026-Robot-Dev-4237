7d79e33eff330562b65eb134cb6d920ad2e3eaaa
greta-20 <greta-20@users.noreply.github.com>
2026-01-17 11:19:52 -0500

    GH AA - added indexer

---
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -0,0 +1,112 @@
+package frc.robot.subsystems;
+
+import static frc.robot.Constants.CANbus.*;
+
+import java.lang.invoke.MethodHandles;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.motors.SparkMaxLance;
+import frc.robot.motors.TalonFXLance;
+
+/**
+ * Indexer
+ */
+public class Indexer extends SubsystemBase
+{
+    // This string gets the full name of the class, including the package name
+    private static final String fullClassName = MethodHandles.lookup().lookupClass().getCanonicalName();
+
+    // *** STATIC INITIALIZATION BLOCK ***
+    // This block of code is run first when the class is loaded
+    static
+    {
+        System.out.println("Loading: " + fullClassName);
+    }
+    
+
+    // *** INNER ENUMS and INNER CLASSES ***
+    // Put all inner enums and inner classes here
+
+
+    
+    // *** CLASS VARIABLES & INSTANCE VARIABLES ***
+    // Put all class variables and instance variables here
+    private final SparkMaxLance motor = new SparkMaxLance(1, ROBORIO, "Motor 1");
+
+    // *** CLASS CONSTRUCTORS ***
+    // Put all class constructors here
+
+    /** 
+     * Creates a new ExampleSubsystem. 
+     */
+    public Indexer()
+    {
+        super("Indexer");
+        System.out.println("  Constructor Started:  " + fullClassName);
+
+        configMotors();
+
+        System.out.println("  Constructor Finished: " + fullClassName);
+    }
+
+
+    // *** CLASS METHODS & INSTANCE METHODS ***
+    // Put all class methods and instance methods here
+
+    private void configMotors()
+    {
+        motor.setupFactoryDefaults();
+    }
+
+    /**
+     * This sets the speed of the motors.
+     * @param speed The motor speed (-1.0 to 1.0)
+     */
+    private void set(double speed)
+    {
+        motor.set(speed);
+    }
+
+    public void stop()
+    {
+        set(0.0);
+    }
+
+    public Command onCommand()
+    {
+        return run( () -> set(0.25) );
+    }
+
+    public Command setCommand(DoubleSupplier speed)
+    {
+        return run( () -> set(MathUtil.clamp(speed.getAsDouble(), 0.0, 0.5)) );
+    }
+
+    // Use a method reference instead of this method
+    public Command stopCommand()
+    {
+        // return run( () -> stop() );
+        return run(this::stop);
+    }
+
+
+    // *** OVERRIDEN METHODS ***
+    // Put all methods that are Overridden here
+
+    @Override
+    public void periodic()
+    {
+        // This method will be called once per scheduler run
+        // Use this for sensors that need to be read periodically.
+        // Use this for data that needs to be logged.
+    }
+
+    @Override
+    public String toString()
+    {
+        return "";
+    }
+}
