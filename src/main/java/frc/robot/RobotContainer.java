19230bbfb033d0e10bdb6570bccb9fc3a0885970
Jon Woodard <jwoodard@lpslancer.net>
2026-01-17 10:14:48 -0500

    fix spelling

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,7 +19,7 @@ public class RobotContainer
     // This string gets the full name of the class including the package name
     private static final String fullClassName = MethodHandles.lookup().lookupClass().getCanonicalName();
 
-    // *** STATTIC INITIALIZATION BLOCK ***
+    // *** STATIC INITIALIZATION BLOCK ***
     // This block of code is run first when the class is loaded
     static
     {
