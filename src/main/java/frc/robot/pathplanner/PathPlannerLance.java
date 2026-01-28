9019e49b6d5163cb088992f3ed5c127f7b33b174
Team 4237 Laptop 4 <LakeshoreRoboticsTeam@gmail.com>
2026-01-28 18:49:00 -0500

    RJ - Created PathPlannerLance

---
+++ src/main/java/frc/robot/pathplanner/PathPlannerLance.java
@@ -0,0 +1,90 @@
+package frc.robot.pathplanner;
+
+import java.lang.invoke.MethodHandles;
+import java.util.Optional;
+import java.util.function.BooleanSupplier;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.Drivetrain;
+
+public class PathPlannerLance
+{
+    // This string gets the full name of the class, including the package name
+    private static final String fullClassName = MethodHandles.lookup().lookupClass().getCanonicalName();
+
+    // *** STATIC INITIALIZATION BLOCK ***
+    // This block of code is run first when the class is loaded
+    static
+    {
+        System.out.println("Loading: " + fullClassName);
+    }
+
+    // *** CLASS VARIABLES & INSTANCE VARIABLES ***
+    // Put all class variables and instance variables here
+
+    private static Drivetrain drivetrain;
+    
+    private static SendableChooser<Command> autoChooser;
+
+
+    private PathPlannerLance()
+    {}
+
+    public static void configPathPlanner(RobotContainer robotContainer)
+    {
+        drivetrain = robotContainer.getDrivetrain();
+
+        configAutoChooser();
+        getAutonomousCommand();
+    }
+
+    private static BooleanSupplier shouldFlipPath()
+    {
+        return 
+        () -> 
+        {
+            Optional<DriverStation.Alliance> alliance = DriverStation.getAlliance();
+            if(alliance.isPresent()) 
+            {
+                return alliance.get() == DriverStation.Alliance.Red;
+            }
+            return false;
+        };
+    }
+    
+    private static void configAutoChooser()
+    {
+        if(AutoBuilder.isConfigured())
+        {
+            autoChooser = AutoBuilder.buildAutoChooser();
+            SmartDashboard.putData("Auto Chooser", autoChooser);
+        }
+        else
+        {
+            autoChooser = new SendableChooser<Command>();
+            autoChooser.setDefaultOption("None", Commands.none());
+        }
+    }
+
+    public static Command getAutonomousCommand() 
+    {
+        if(autoChooser != null)
+        {
+            return autoChooser.getSelected();
+        }
+        else
+        {
+            return Commands.none();
+        }
+        // return new PathPlannerAuto("TEST - Move 2m");
+
+    }
+}
